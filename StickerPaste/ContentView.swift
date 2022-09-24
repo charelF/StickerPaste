d37197049a33bf7ab51496a4a40cc0c5e8f64662
Charel Felten <charel.felten@gmail.com>
2022-09-24 20:25:31 +0200

    StickerApp initial push

--- StickerPaste/ContentView.swift
+++ StickerPaste/ContentView.swift
@@ -8,9 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var stickers : [UIImage] = []
+    
+    private func pasteImage() {
+        if let image = UIPasteboard.general.image {
+            stickers.append(image)
+        }
+    }
+    
+    private func saveImage() {
+        let image = self.body.snapshot()
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.black
+            ZStack {
+                Color.white
+                
+                
+                ZStack {
+                    ForEach(stickers, id: \.self) {sticker in
+                        StickerView(sticker: sticker)
+                    }
+                }
+                
+            }
+            .contextMenu {
+                Button {
+                    pasteImage()
+                } label: {
+                    Label("Paste Sticker", systemImage: "photo")
+                }
+                Button {
+                    saveImage()
+                } label: {
+                    Label("Save Collage", systemImage: "square.and.arrow.down.on.square")
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct StickerView: View {
+    
+    @State private var position:CGPoint = CGPoint(x: 0, y: 0)
+    
+    private var sticker: UIImage
+    
+    @State var scale: CGFloat = 1.0
+    
+    init(sticker: UIImage) {
+        self.sticker = sticker
+    }
+    
+    var body: some View {
+        Image(uiImage: sticker)
+//        Rectangle()
+            .resizable()
+            .position(position)
+            .frame(alignment: .center)
+            .hoverEffect()
+        
+            .onTapGesture(coordinateSpace: .global) { location in
+                    print("Tapped at \(location)")
+                }
+            .gesture(
+                DragGesture()
+                    .onChanged({ (gesture) in
+                        position = gesture.location
+                    })
+//                    .onEnded({ (gesture) in
+//                        position = gesture.location
+//                    })
+            )
+           .scaleEffect(scale)
+           .frame(width: 100, height: 100)
+           .gesture(MagnificationGesture()
+               .onChanged { value in
+                   self.scale = value.magnitude
+               }
+           )
+    }
+}
+
+extension View {
+    // https://www.hackingwithswift.com/quick-start/swiftui/how-to-convert-a-swiftui-view-to-an-image
+    func snapshot() -> UIImage {
+        let controller = UIHostingController(rootView: self)
+        let view = controller.view
+
+        let targetSize = controller.view.intrinsicContentSize
+        view?.bounds = CGRect(origin: .zero, size: targetSize)
+        view?.backgroundColor = .clear
+
+        let renderer = UIGraphicsImageRenderer(size: targetSize)
+
+        return renderer.image { _ in
+            view?.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
+        }
     }
 }
 

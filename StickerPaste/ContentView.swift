4a3ddc555ab0da4bf070f49efa5e58e9be744666
Charel Felten <charel.felten@gmail.com>
2022-09-24 17:18:55 +0200

    Initial Commit

---
+++ StickerPaste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StickerPaste
+//
+//  Created by Charel Felten on 24/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StickerPaste/StickerPasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickerPasteApp.swift
+//  StickerPaste
+//
+//  Created by Charel Felten on 24/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct StickerPasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

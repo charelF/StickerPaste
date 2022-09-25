b5de1e8e8627911a026f1cddffeac01064fb3db1
Charel Felten <charel.felten@gmail.com>
2022-09-25 22:44:32 +0200

    got gestures working nicely

--- StickerPaste/ContentView.swift
+++ StickerPaste/ContentView.swift
@@ -12,12 +12,16 @@ struct ContentView: View {
     @State private var stickers : [UIImage] = []
 //    @State private var stickerZ: [Int]
     
-    private func pasteImage() {
+    private func pasteSticker() {
         if let image = UIPasteboard.general.image {
             stickers.append(image)
         } else {
             stickers.append(UIImage(systemName: "questionmark")!)
         }
+    }
+    
+    private func deleteSticker(_ stickerID: UUID) {
+        
     }
     
     private func saveImage() {
@@ -35,6 +39,24 @@ struct ContentView: View {
                 ZStack {
                     ForEach(stickers, id: \.self) {sticker in
                         StickerView(sticker: sticker)
+//                            .on
+                            .contextMenu {
+                                Button {
+                                    pasteSticker()
+                                } label: {
+                                    Label("Paste Sticker", systemImage: "photo")
+                                }
+                                Button {
+                                    saveImage()
+                                } label: {
+                                    Label("Save Collage", systemImage: "square.and.arrow.down.on.square")
+                                }
+                                Button {
+//                                    deleteSticker(sticker.id)
+                                } label: {
+                                    Label("Remove Sticker", systemImage: "trash")
+                                }
+                            }
 //                            .zIndex(<#T##value: Double##Double#>)
                     }
                 }
@@ -42,7 +64,7 @@ struct ContentView: View {
             }
             .contextMenu {
                 Button {
-                    pasteImage()
+                    pasteSticker()
                 } label: {
                     Label("Paste Sticker", systemImage: "photo")
                 }
@@ -53,30 +75,42 @@ struct ContentView: View {
                 }
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+        .background(Color.red)
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct StickerView: View {
     
-    @State private var position:CGPoint = CGPoint(x: 0, y: 0)
+    @State var position: CGPoint
+    @State var startLocation: CGPoint
+    @State var initialDifference: CGPoint
+    @State var isDraging: Bool = false
+    @State var width: CGFloat
+    @State var height: CGFloat
+    @State var scale: CGFloat = 1
+    @State var angle = Angle(degrees: 0.0)
+    @State var zindex = 1
     
     private var sticker: UIImage
-    
-    @State var scale: CGFloat = 1
-    
-    @State var angle = Angle(degrees: 0.0)
-    
-    @State var zindex = 1
+    var id: String
     
     init(sticker: UIImage) {
         self.sticker = sticker
+        self.id = UUID().uuidString
         self.scale = sticker.scale
+        self.position = CGPoint(x: 0, y: 0)
+        self.startLocation = CGPoint(x: 0, y: 0)
+        self.initialDifference = CGPoint(x: 0, y: 0)
+        self.width = 200
+        self.height = 200
     }
     
     var rotationGesture: some Gesture {
         RotationGesture()
             .onChanged { angle in
+                print("RotationGesture \(angle)")
                 self.angle = angle
             }
     }
@@ -84,28 +118,71 @@ struct StickerView: View {
     var magnificationGesture: some Gesture {
         MagnificationGesture()
             .onChanged { scale in
-                print("zoom")
+                print("MagnificationGesture \(scale)")
                 self.scale = scale
             }
     }
     
     var dragGesture: some Gesture {
         DragGesture()
-            .onChanged({ (gesture) in
-                print("drag")
-                self.position = gesture.location
-            })
+            .onChanged { value in
+                print("DragGesture START \(value)")
+//                print(self.startLocation)
+//                print(value.location)
+//                print(value.startLocation)
+                
+                if !self.isDraging {
+                    initialDifference.x = value.startLocation.x - self.position.x
+                    initialDifference.y = value.startLocation.y - self.position.y
+                    self.isDraging = true
+                }
+                
+                self.position.x = value.location.x - initialDifference.x
+                self.position.y = value.location.y - initialDifference.y
+                
+                print("initial difference \(self.initialDifference)")
+                print("gesture start location \(value.startLocation)")
+                print("position \(self.position)")
+                
+//                var touchToCenterDiff: CGPoint
+//                touchToCenterDiff.x = value.startLocation.x - self.startLocation.x
+//                touchToCenterDiff.y = value.startLocation.y - self.startLocation.y
+//
+//                var newPosition: CGPoint
+//                newPosition.x = value.location -
+                
+                
+                
+                
+//                self.position.x = self.position.x + value.location.x - value.startLocation.x
+//                self.position.y = self.position.y + value.location.y - value.startLocation.y
+//                value.startLocation
+                
+                
+//                var diff: CGPoint
+//                diff.x = = value.startLocation.first - self.startLocation.first
+                
+//                self.position = value.location
+            }
+            .onEnded { value in
+                print("DragGesture END \(value)")
+                self.startLocation = position
+                self.isDraging = false
+            }
     }
     
     var body: some View {
+//            Image(uiImage: sticker)
+        ZStack {
+            Rectangle()
+                .fill(Color.black.opacity(0.1))
             Image(uiImage: sticker)
-                .resizable()
+        }
+//            .resizable()
                 .position(position)
-                .frame(alignment: .center)
-                .hoverEffect()
+//                .frame(width: self.width, height: self.height, alignment: .center)
                 .rotationEffect(self.angle)
-                .scaleEffect(scale)
-                .frame(width: sticker.size.width/5, height: sticker.size.height/5)
+                .scaleEffect(self.scale)
                 .gesture(
                     dragGesture
                     .simultaneously(with: rotationGesture)
---
+++ StickerPaste/SampleGestureView.swift
@@ -0,0 +1,79 @@
+//
+//  SampleGestureView.swift
+//  StickerPaste
+//
+//  Created by Charel Felten on 25/09/2022.
+//
+
+import SwiftUI
+
+struct SampleGestureView: View {
+    
+    @State var previousScale: CGFloat = 1
+    @State var newScale: CGFloat = 1
+    @State var previousAngle: Angle = Angle(degrees: 0.0)
+    @State var newAngle: Angle = Angle(degrees: 0.0)
+    @State var previousPosition: CGPoint = CGPoint(x: 0, y: 0)
+    @State var newPosition: CGPoint = CGPoint(x: 0, y: 0)
+    @State var positionDifference: CGPoint = CGPoint(x: 0, y: 0)
+    @State var positionMovement: CGPoint = CGPoint(x: 0, y: 0)
+    
+    var magnificationGesture: some Gesture {
+        MagnificationGesture()
+            .onChanged { value in
+                newScale = previousScale * value
+            }
+            .onEnded { value in
+                previousScale *= value
+            }
+    }
+    
+    var rotationGesture: some Gesture {
+        RotationGesture()
+            .onChanged { value in
+                newAngle.degrees = previousAngle.degrees + value.degrees
+            }
+            .onEnded { value in
+                previousAngle.degrees += value.degrees
+            }
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+                positionMovement.x = value.startLocation.x - value.location.x
+                positionMovement.y = value.startLocation.y - value.location.y
+                newPosition.x = previousPosition.x - positionMovement.x
+                newPosition.y = previousPosition.y - positionMovement.y
+            }
+            .onEnded { value in
+                previousPosition = newPosition
+            }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("newScale \(newScale)")
+            Text("newAngle \(newAngle.degrees)")
+            Text("newPosition \(newPosition.x), \(newPosition.y)")
+            Spacer()
+            Rectangle()
+                .fill(Color.red)
+                .rotationEffect(newAngle)  // needs to be before scaleEffect
+                .scaleEffect(newScale)
+                .position(newPosition)
+                .gesture(
+                    rotationGesture
+                        .simultaneously(with: magnificationGesture)
+                        .simultaneously(with: dragGesture)
+                )
+            Spacer()
+        }
+    }
+}
+
+struct SampleGestureView_Previews: PreviewProvider {
+    static var previews: some View {
+        SampleGestureView()
+    }
+}

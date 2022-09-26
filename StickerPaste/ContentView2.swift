008d294b2c5402691b24940cec92328b61ecb992
Charel Felten <charel.felten@gmail.com>
2022-09-26 11:50:31 +0200

    content view 2 looking good

--- StickerPaste/ContentView2.swift
+++ StickerPaste/ContentView2.swift
@@ -11,6 +11,8 @@ struct ContentView2: View {
     
     @State private var stickers : [UIImage] = []
     
+    @State private var backgroundColor: Color = Color.white
+    
     func pasteSticker() {
         if let image = UIPasteboard.general.image {
             stickers.append(image)
@@ -19,30 +21,65 @@ struct ContentView2: View {
         }
     }
     
-    func deleteSticker(_ id: UUID) {
-        
-//        for i in 0..<stickers.count {
-//            if stickers[i].id == id {
-//                stickers.remove(at: i)
-//                break
-//            }
-//        }
+    func changeBackgroundColor() {
         
     }
     
-    var body: some View {
-
-        ZStack {
-            Color.white
-            ForEach(stickers, id: \.self) { sticker in
-                StickerView2(deleteSticker: deleteSticker, sticker: sticker)
-            }
-        }
-        .contextMenu {
+    func deleteSticker(_ id: UUID) {
+        
+        //        for i in 0..<stickers.count {
+        //            if stickers[i].id == id {
+        //                stickers.remove(at: i)
+        //                break
+        //            }
+        //        }
+        
+    }
+    
+    var mainMenu: some View {
+        Group {
             Button {
                 pasteSticker()
             } label: {
                 Label("Paste Sticker", systemImage: "photo")
+            }
+            Button {
+                deleteSticker(UUID())
+            } label: {
+                Label("Save Collage", systemImage: "square.and.arrow.down.on.square")
+            }
+            Menu("Background Color") {
+                Picker(selection: $backgroundColor, label: Text("Background Color")) {
+                    Text("White").tag(Color.white)
+                    Text("Black").tag(Color.black)
+                    Text("Red").tag(Color.red)
+                    Text("Yellow").tag(Color.yellow)
+                    Text("Green").tag(Color.green)
+                    Text("Blue").tag(Color.blue)
+                }
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationView() {
+            ZStack {
+                backgroundColor
+                ForEach(stickers, id: \.self) { sticker in
+                    StickerView2(deleteSticker: deleteSticker, sticker: sticker)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Menu(content: {
+                        mainMenu
+                    }, label: {
+                        Label("Add", systemImage: "plus").foregroundColor(Color.black)
+                    })
+                }
+            }
+            .contextMenu {
+                mainMenu
             }
         }
     }
